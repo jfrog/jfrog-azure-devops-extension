e4b963c34c68fe8aa77499a3955755624a373e7d
yahavi <yahavizhak@gmail.com>
2018-07-30 14:14:39 +0300

    Add support for using a custom cli path

--- jfrog-utils/jutils.js
+++ jfrog-utils/jutils.js
@@ -7,18 +7,21 @@ const execSync = require('child_process').execSync;
 
 const fileName = getFileName();
 const btPackage = "jfrog-cli-" + getArchitecture();
-const folderPath = path.join(tl.getVariable("Agent.WorkFolder"), "_jfrog");
+const jfrogFolderPath = path.join(tl.getVariable("Agent.WorkFolder"), "_jfrog");
 const version = "1.17.1";
-const cliPath = path.join(folderPath, version, fileName);
-const cliTmpPath = cliPath + ".tmp";
+const versionedCliPath = path.join(jfrogFolderPath, version, fileName);
+const customCliPath = path.join(jfrogFolderPath, "current", fileName);
 const cliUrl = 'https://api.bintray.com/content/jfrog/jfrog-cli-go/' + version + '/' + btPackage + '/' + fileName + "?bt_package=" + btPackage;
-const MAX_RETRIES = 10;
+const MAX_CLI_DOWNLOADS_RETRIES = 10;
+const DOWNLOAD_CLI_ERR = "Failed while attempting to download JFrog CLI from " + cliUrl +
+    ". If this build agent is not accessible to the internet, you can manually place version " + version +
+    " of JFrog CLI on the agent in the following path: " + customCliPath;
 
 let runTaskCbk = null;
 
 module.exports = {
-    executeCliCommand: executeCliCommand,
     executeCliTask: executeCliTask,
+    executeCliCommand: executeCliCommand,
     handleException: handleException,
     cliJoin: cliJoin,
     quote: quote,
@@ -27,18 +30,35 @@ module.exports = {
     addBoolParam: addBoolParam
 };
 
-function executeCliTask (runTaskFunc) {
-    process.env.JFROG_CLI_HOME = path.join(folderPath, version);
+function executeCliTask(runTaskFunc) {
+    process.env.JFROG_CLI_HOME = jfrogFolderPath;
     process.env.JFROG_CLI_OFFER_CONFIG = false;
 
     runTaskCbk = runTaskFunc;
-    if (!fs.existsSync(cliPath)) {
-        createCliDirs();
-        downloadCli().then(runCbk)
+    if (fs.existsSync(customCliPath)) {
+        runCbk(customCliPath);
+    } else if (fs.existsSync(versionedCliPath)) {
+        runCbk(versionedCliPath);
     } else {
-        console.log("JFrog CLI  " + version + " exists locally.");
-        runCbk();
+        createCliDirs();
+        downloadCli().then(() => {
+            runCbk(versionedCliPath);
+        });
     }
+}
+
+function executeCliCommand(cliCommand, runningDir) {
+    try {
+        execSync(cliCommand, {cwd: runningDir, stdio: [0, 1, 2]});
+    } catch (ex) {
+        // Error occurred
+        handleException(ex);
+    }
+}
+
+function handleException(ex) {
+    tl.setResult(tl.TaskResult.Failed, ex);
+    process.exit(1);
 }
 
 function cliJoin() {
@@ -51,7 +71,6 @@ function cliJoin() {
     }
     return command;
 }
-
 
 function quote(str) {
     return "\"" + str + "\"";
@@ -70,75 +89,6 @@ function addArtifactoryCredentials(cliCommand, artifactoryService) {
     return cliCommand
 }
 
-function executeCliCommand(cliCommand, runningDir) {
-    try {
-        execSync(cliCommand, {cwd:runningDir, stdio:[0,1,2]});
-    } catch (ex) {
-        // Error occurred
-        handleException(ex);
-    }
-}
-
-function handleException (ex) {
-    tl.setResult(tl.TaskResult.Failed, ex);
-    process.exit(1);
-}
-
-function runCbk() {
-    if (runTaskCbk != null) {
-        runTaskCbk(cliPath);
-    }
-}
-
-function createCliDirs() {
-    if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath);
-    }
-
-    if (!fs.existsSync(path.join(folderPath, version))) {
-        fs.mkdirSync(path.join(folderPath, version));
-    }
-}
-
-function downloadCli(attemptNumber) {
-    return new Promise((resolve, reject) => {
-        function handleError(err) {
-            if (attemptNumber <= MAX_RETRIES) {
-                console.log("Attempt #" + attemptNumber + " to download jfrog-cli failed, trying again.");
-                downloadCli(++attemptNumber);
-            } else {
-                reject(err);
-            }
-        }
-
-        let req = requestPromise.get(cliUrl);
-        req.on('response', (res) => {
-            res.pipe(
-                checksumStream({
-                    algorithm: 'sha256',
-                    digest: res.headers['X-Checksum-Sha256'],
-                    size: res.headers['content-length']
-                }).on('error', handleError)
-                    .on('end', () => {
-                        if (res.statusCode >= 200 && res.statusCode < 300) {
-                            fs.rename(cliTmpPath, cliPath, () => {
-                                if (!process.platform.startsWith("win")) {
-                                    fs.chmodSync(cliPath, 0o555)
-                                }
-                                console.log("Finished downloading jfrog cli");
-                                resolve();
-                            });
-                        }
-                    })
-            ).pipe(
-                fs.createWriteStream(cliTmpPath)
-            )
-        }).catch(handleError)
-    }).catch((err) => {
-        tl.setResult(tl.TaskResult.Failed, err.message);
-    });
-}
-
 function addStringParam(cliCommand, inputParam, cliParam) {
     let val = tl.getInput(inputParam, false);
     if (val !== null) {
@@ -151,6 +101,78 @@ function addBoolParam(cliCommand, inputParam, cliParam) {
     let val = tl.getBoolInput(inputParam, false);
     cliCommand = cliJoin(cliCommand, "--" + cliParam + "=" + val);
     return cliCommand
+}
+
+function checkCliVersion(cliPath) {
+    let cliCommand = cliJoin(cliPath, "--version");
+    try {
+        let res = execSync(cliCommand);
+        let detectedVersion = String.fromCharCode.apply(null, res).split(' ')[2].trim();
+        if (detectedVersion === version) {
+            console.log("JFrog CLI version: " + detectedVersion);
+        } else {
+            console.warn("Expected to find version " + version + " of JFrog CLI at " + cliPath + ". Found version " + detectedVersion + " instead.");
+        }
+    } catch (ex) {
+        console.error("Failed to get JFrog CLI version: " + ex);
+    }
+}
+
+function runCbk(cliPath) {
+    console.log("Running jfrog-cli from " + cliPath + ".");
+    checkCliVersion(cliPath);
+    runTaskCbk(cliPath)
+}
+
+function createCliDirs() {
+    if (!fs.existsSync(jfrogFolderPath)) {
+        fs.mkdirSync(jfrogFolderPath);
+    }
+
+    if (!fs.existsSync(path.join(jfrogFolderPath, version))) {
+        fs.mkdirSync(path.join(jfrogFolderPath, version));
+    }
+}
+
+function downloadCli(attemptNumber) {
+    return new Promise((resolve, reject) => {
+        let handleError = (err) => {
+            if (attemptNumber <= MAX_CLI_DOWNLOADS_RETRIES) {
+                console.log("Attempt #" + attemptNumber + " to download jfrog-cli failed, trying again.");
+                downloadCli(++attemptNumber);
+            } else {
+                console.error(DOWNLOAD_CLI_ERR);
+                reject(err);
+            }
+        };
+        const cliTmpPath = versionedCliPath + ".tmp";
+        let req = requestPromise.get(cliUrl);
+        req.on('response', (res) => {
+            res.pipe(
+                checksumStream({
+                    algorithm: 'sha256',
+                    digest: res.headers['X-Checksum-Sha256'],
+                    size: res.headers['content-length']
+                }).on('error', handleError)
+                    .on('end', () => {
+                        if (res.statusCode >= 200 && res.statusCode < 300) {
+                            fs.rename(cliTmpPath, versionedCliPath, () => {
+                                if (!process.platform.startsWith("win")) {
+                                    fs.chmodSync(versionedCliPath, 0o555)
+                                }
+                                console.log("Finished downloading jfrog cli");
+                                resolve();
+                            });
+                        }
+                    })
+            ).pipe(
+                fs.createWriteStream(cliTmpPath)
+            )
+        }).catch(handleError)
+    }).catch((err) => {
+        console.error(DOWNLOAD_CLI_ERR);
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    });
 }
 
 function getArchitecture() {
