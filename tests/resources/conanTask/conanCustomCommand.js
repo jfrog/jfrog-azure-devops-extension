e57ff181837e1f6aac497dfc0fe9a2908587efac
Elio Marcolino <eliom@jfrog.com>
2018-08-13 13:08:13 -0700

    Added Conan Support

---
+++ tests/resources/conanTask/conanCustomCommand.js
@@ -0,0 +1,20 @@
+const testUtils = require('../../testUtils');
+const path = require('path');
+
+const TEST_NAME = path.basename(__dirname);
+const BUILD_NAME = TEST_NAME
+const BUILD_NUMBER = "1"
+
+let variables = {
+    "Build.DefinitionName": BUILD_NAME,
+    "Build.BuildDirectory": "/tmp/" + BUILD_NAME,
+    "Build.BuildNumber": BUILD_NUMBER
+};
+
+let inputs = {
+    "conanCommand": "Custom",
+    "customArguments": "remote list",
+    "conanUserHome": "/tmp/" + BUILD_NAME + "/" + BUILD_NUMBER
+};
+
+testUtils.runTask(testUtils.conan, variables, inputs);
