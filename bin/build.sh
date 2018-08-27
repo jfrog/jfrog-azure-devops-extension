2a112d425b395821cd98de2f62e0b4dae8ee4d0c
yahavi <yahavizhak@gmail.com>
2018-08-27 14:39:19 +0300

    Structural refactor

--- bin/build.sh
+++ bin/build.sh
@@ -10,7 +10,7 @@ npm pack
 cd ..
 
 echo In path: $(pwd)
-declare -a arr=("ArtifactoryBuildPromotion" "ArtifactoryGenericDownload" "ArtifactoryGenericUpload" "ArtifactoryPublishBuildInfo" "ArtifactoryNpm")
+declare -a arr=("ArtifactoryBuildPromotion" "ArtifactoryGenericDownload" "ArtifactoryGenericUpload" "ArtifactoryNpm" "ArtifactoryPublishBuildInfo")
 
 for i in "${arr[@]}"
 do
