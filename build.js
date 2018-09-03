28c4554c3eaf69ae38a7e8e8854af97029e96036
dimanevelev <dimanevelev@gmail.com>
2018-09-03 22:58:23 +0300

    Conan publish publish buildInfo changes

--- build.js
+++ build.js
@@ -1,17 +1,21 @@
 let os = require('os');
 let exec = require('child_process').exec;
-function puts(error, stdout, stderr) { console.log(stdout) }
 
 switch (os.type()) {
     case 'Linux':
-        exec("bin/build.sh", puts);
+        exec("bin/build.sh", printOutput);
         break;
     case 'Darwin':
-        exec("bin/build.sh", puts);
+        exec("bin/build.sh", printOutput);
         break;
     case 'Windows_NT':
-        exec("bin\\build.bat", puts);
+        exec("bin\\build.bat", printOutput);
         break;
     default:
         throw new Error("Unsupported OS found: " + os.type());
+}
+
+function printOutput(stdout, stderr) {
+    console.log(stdout);
+    console.error(stderr);
 }
