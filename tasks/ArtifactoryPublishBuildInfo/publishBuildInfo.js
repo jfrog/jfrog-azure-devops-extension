90ac0f041a265a7b107d31963d577db845cfb39f
Yahav Itzhak <yahavi@users.noreply.github.com>
2018-10-02 15:58:56 +0300

    Add build url

--- tasks/ArtifactoryPublishBuildInfo/publishBuildInfo.js
+++ tasks/ArtifactoryPublishBuildInfo/publishBuildInfo.js
@@ -18,11 +18,9 @@ function RunTaskCbk(cliPath) {
     let artifactoryService = tl.getInput("artifactoryService", false);
     let artifactoryUrl = tl.getEndpointUrl(artifactoryService, false);
     let excludeEnvVars = tl.getInput("excludeEnvVars", false);
-    if (!excludeEnvVars) {
-        excludeEnvVars = "_"; // This is a workaround - jfrog-cli v1.18 doesn't support empty env-exclude patterns.
-    }
 
     let cliCommand = utils.cliJoin(cliPath, cliBuildPublishCommand, utils.quote(buildDefinition), utils.quote(buildNumber), "--url=" + utils.quote(artifactoryUrl), "--env-exclude=" + utils.quote(excludeEnvVars));
+    cliCommand = addBuildUrl(cliCommand);
     cliCommand = utils.addArtifactoryCredentials(cliCommand, artifactoryService);
 
     let taskRes = utils.executeCliCommand(cliCommand, workDir);
@@ -48,6 +46,17 @@ function attachBuildInfoUrl(buildName, buildNumber, workDir) {
 
     //Executes command to attach the file to build
     console.log("##vso[task.addattachment type=artifactoryType;name=buildDetails;]" + artifactoryUrlFile);
+}
+
+function addBuildUrl(cliCommand) {
+    let collectionUri = tl.getVariable('System.TeamFoundationCollectionUri');
+    let projectName = tl.getVariable('System.TeamProject');
+    let buildId = tl.getVariable('Build.BuildId');
+    let releaseId = tl.getVariable('Release.ReleaseId');
+
+    let buildUrl = collectionUri + projectName + "/_" + (releaseId ? "release?releaseId=" + releaseId : "build?buildId=" + buildId);
+    cliCommand = utils.cliJoin(cliCommand, "--build-url=" + utils.quote(buildUrl));
+    return cliCommand;
 }
 
 utils.executeCliTask(RunTaskCbk);
