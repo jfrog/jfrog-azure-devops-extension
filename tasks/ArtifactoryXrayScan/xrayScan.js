2a98053901d48f827de495c4279af1eaf0615a81
barbelity <barb@jfrog.com>
2019-10-30 14:09:28 +0200

    Artifactory Tab: Add build-scan report icon and link

--- tasks/ArtifactoryXrayScan/xrayScan.js
+++ tasks/ArtifactoryXrayScan/xrayScan.js
@@ -1,6 +1,7 @@
 
 const tl = require('azure-pipelines-task-lib/task');
 const utils = require('artifactory-tasks-utils');
+const path = require('path');
 
 const cliXrayScanCommand = "rt bs";
 
@@ -18,9 +19,21 @@ function RunTaskCbk(cliPath) {
 
     try {
         let taskRes = utils.executeCliCommand(cliCommand, process.cwd(), 'pipe');
-        console.log(taskRes);
-        // Check if should fail build.
+        console.log("Scan result:\n" + taskRes);
         let resJson = JSON.parse(taskRes);
+        let scanUrl = resJson['summary']['more_details_url'];
+
+        // Save build-scan link in file.
+        let workDir = tl.getVariable('System.DefaultWorkingDirectory');
+        if (!workDir) {
+            tl.setResult(tl.TaskResult.Failed, "Failed getting default working directory.");
+            return;
+        }
+        let scanUrlFile = path.join(workDir, "scanUrlFile");
+        tl.writeFile(scanUrlFile, JSON.stringify(scanUrl));
+        console.log("##vso[task.addattachment type=xrayType;name=scanDetails;]" + scanUrlFile);
+
+        // Check if should fail build.
         if (allowFailBuild) {
             let buildFailed = resJson['summary']['fail_build'];
             if (buildFailed) {
