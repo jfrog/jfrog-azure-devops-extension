dff57f3d45224e0ea0facd7a6e919ee27622ccb3
barbelity <barb@jfrog.com>
2019-11-27 11:19:31 +0200

    Update jfrog-cli version

--- artifactory-tasks-utils/utils.js
+++ artifactory-tasks-utils/utils.js
@@ -11,8 +11,8 @@ const fileName = getCliExecutableName();
 const toolName = "jfrog";
 const btPackage = "jfrog-cli-" + getArchitecture();
 const jfrogFolderPath = encodePath(path.join(tl.getVariable("Agent.WorkFolder"), "_jfrog"));
-const jfrogCliVersion = "1.31.0";
-const pluginVersion = "1.8.0";
+const jfrogCliVersion = "1.31.2";
+const pluginVersion = "1.8.1";
 const buildAgent = "artifactory-azure-devops-extension";
 const customCliPath = encodePath(path.join(jfrogFolderPath, "current", fileName)); // Optional - Customized jfrog-cli path.
 const jfrogCliBintrayDownloadUrl = 'https://api.bintray.com/content/jfrog/jfrog-cli-go/' + jfrogCliVersion + '/' + btPackage + '/' + fileName + "?bt_package=" + btPackage;
