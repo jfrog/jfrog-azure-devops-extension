6d4f9e3a37da7c18f1fa2a03cf8cb1e7b9940095
Bar Belity <barb@jfrog.com>
2020-03-22 17:58:28 +0200

    Change CLI download dir to 'Agent.ToolsDirectory' (#125)

--- artifactory-tasks-utils/utils.js
+++ artifactory-tasks-utils/utils.js
@@ -10,11 +10,14 @@ const yaml = require('js-yaml');
 const fileName = getCliExecutableName();
 const toolName = 'jfrog';
 const btPackage = 'jfrog-cli-' + getArchitecture();
-const jfrogFolderPath = encodePath(path.join(tl.getVariable('Agent.WorkFolder'), '_jfrog'));
+const jfrogFolderPath = encodePath(path.join(tl.getVariable('Agent.ToolsDirectory'), '_jfrog'));
+const jfrogLegacyFolderPath = encodePath(path.join(tl.getVariable('Agent.WorkFolder'), '_jfrog'));
 const jfrogCliVersion = '1.31.2';
 const pluginVersion = '1.8.1';
 const buildAgent = 'artifactory-azure-devops-extension';
-const customCliPath = encodePath(path.join(jfrogFolderPath, 'current', fileName)); // Optional - Customized jfrog-cli path.
+const customFolderPath = encodePath(path.join(jfrogFolderPath, 'current'));
+const customCliPath = encodePath(path.join(customFolderPath, fileName)); // Optional - Customized jfrog-cli path.
+const customLegacyCliPath = encodePath(path.join(jfrogLegacyFolderPath, 'current', fileName));
 const jfrogCliBintrayDownloadUrl =
     'https://api.bintray.com/content/jfrog/jfrog-cli-go/' + jfrogCliVersion + '/' + btPackage + '/' + fileName + '?bt_package=' + btPackage;
 const buildToolsConfigVersion = 1;
@@ -48,7 +51,8 @@ module.exports = {
     appendBuildFlagsToCliCommand: appendBuildFlagsToCliCommand
 };
 
-// Url and AuthHandlers are optional. Using jfrogCliBintrayDownloadUrl by default.
+// The cliDownloadUrl and cliAuthHandlers arguments are optional. They are provided to this function by the 'Artifactory Tools Installer' task.
+// jfrogCliBintrayDownloadUrl is used by default.
 function executeCliTask(runTaskFunc, cliDownloadUrl, cliAuthHandlers) {
     process.env.JFROG_CLI_HOME = jfrogFolderPath;
     process.env.JFROG_CLI_OFFER_CONFIG = false;
@@ -68,12 +72,18 @@ function executeCliTask(runTaskFunc, cliDownloadUrl, cliAuthHandlers) {
         .catch(error => tl.setResult(tl.TaskResult.Failed, 'Error occurred while executing task:\n' + error));
 }
 
-// Url and AuthHandlers are optional. Using jfrogCliBintrayDownloadUrl by default.
 function getCliPath(cliDownloadUrl, cliAuthHandlers) {
     return new Promise(function(resolve, reject) {
         let cliDir = toolLib.findLocalTool(toolName, jfrogCliVersion);
         if (fs.existsSync(customCliPath)) {
             tl.debug('Using cli from custom cli path: ' + customCliPath);
+            resolve(customCliPath);
+        } else if (fs.existsSync(customLegacyCliPath)) {
+            tl.warning(
+                'Found JFrog CLI in deprecated custom path: ' + customLegacyCliPath + '. Copying JFrog CLI to new supported path: ' + customFolderPath
+            );
+            tl.mkdirP(customFolderPath);
+            tl.cp(customLegacyCliPath, customFolderPath, '-f');
             resolve(customCliPath);
         } else if (cliDir) {
             let cliPath = path.join(cliDir, fileName);
@@ -294,7 +304,6 @@ function createCliDirs() {
     }
 }
 
-// Url and AuthHandlers are optional. Using jfrogCliBintrayDownloadUrl by default.
 function downloadCli(cliDownloadUrl, cliAuthHandlers) {
     // If unspecified, use the default cliDownloadUrl of Bintray.
     if (!cliDownloadUrl) {
