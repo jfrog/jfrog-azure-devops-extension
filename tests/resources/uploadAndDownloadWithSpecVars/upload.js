8f6a5fd35ab5a8692c66f2d075d1a207cd1a8915
Liron Shalom <lirons@jfrog.com>
2020-03-26 18:21:43 +0200

    fix specVars test

--- tests/resources/uploadAndDownloadWithSpecVars/upload.js
+++ tests/resources/uploadAndDownloadWithSpecVars/upload.js
@@ -1,17 +1,14 @@
 const testUtils = require('../../testUtils');
-const tasksUtils = require('artifactory-tasks-utils');
-
 const TEST_NAME = testUtils.getTestName(__dirname);
 
-let patternValue = tasksUtils.fixWindowsPaths(testUtils.getTestLocalFilesDir(__dirname));
-let targetValue = tasksUtils.fixWindowsPaths(testUtils.getRemoteTestDir(testUtils.getRepoKeys().repo1, TEST_NAME));
+let patternValue = testUtils.getTestLocalFilesDir(__dirname);
 
 let inputs = {
     fileSpec: JSON.stringify({
         files: [
             {
                 pattern: '${patternVar}',
-                target: '${targetVar}'
+                target: testUtils.getRemoteTestDir(testUtils.getRepoKeys().repo1, TEST_NAME)
             }
         ]
     }),
@@ -21,7 +18,7 @@ let inputs = {
     symlinks: false,
     specSource: 'taskConfiguration',
     replaceSpecVars: true,
-    specVars: 'patternVar=' + patternValue + ';targetVar=' + targetValue
+    specVars: 'patternVar=' + patternValue
 };
 
 testUtils.runTask(testUtils.upload, {}, inputs);
