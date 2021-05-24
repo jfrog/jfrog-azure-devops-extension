3755b5d798c16f578049253cce2d32af3a0ea1d4
Robi Nino <robi.nino@gmail.com>
2021-05-24 20:15:56 +0300

    Update publish-private.sh due to the removal of publisher API

--- buildScripts/publish-private.sh
+++ buildScripts/publish-private.sh
@@ -6,6 +6,7 @@
 # Precondition: Set 2 environment variables:
 # 1. ADO_ARTIFACTORY_DEVELOPER - Developer ID in Visual Studio (Organization name in Azure DevOps)
 # 2. ADO_ARTIFACTORY_API_KEY - API key in Visual Studio (Personal access tokens under 'Security' menu, configured to 'All accessible organizations' in 'Organization' and 'Full access' in 'Scopes'.)
+# 3. Create publisher at https://aka.ms/vsm-create-publisher with the Name (and ID): '$ADO_ARTIFACTORY_DEVELOPER-private' (other details are not required).
 
 if [ -z "$ADO_ARTIFACTORY_DEVELOPER" ]; then
     echo "Please set ADO_ARTIFACTORY_DEVELOPER first"
@@ -27,8 +28,6 @@ sed -i '' "s/\"version\": \".*..*..*\"/\"version\": \"${RANDOM}.${RANDOM}.${RAND
 
 
 tfx extension unshare -t $ADO_ARTIFACTORY_API_KEY --extension-id jfrog-artifactory-vsts-extension --publisher $PUBLISHER --unshare-with $ADO_ARTIFACTORY_DEVELOPER 2>/dev/null
-tfx extension publisher delete -t $ADO_ARTIFACTORY_API_KEY --publisher $PUBLISHER 2>/dev/null
-tfx extension publisher create -t $ADO_ARTIFACTORY_API_KEY --publisher $PUBLISHER --display-name $PUBLISHER --description "Publishes Artifactory extension privately for sanity tests"
 tfx extension create --manifest-globs vss-extension-private.json --publisher $PUBLISHER
 tfx extension publish -t $ADO_ARTIFACTORY_API_KEY --publisher $PUBLISHER --manifests vss-extension-private.json --override "{\"public\": false}" --share-with $ADO_ARTIFACTORY_DEVELOPER
 tfx extension install --publisher $PUBLISHER --extension-id jfrog-artifactory-vsts-extension --service-url https://$ADO_ARTIFACTORY_DEVELOPER.visualstudio.com -t $ADO_ARTIFACTORY_API_KEY
