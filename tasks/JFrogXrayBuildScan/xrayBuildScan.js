bb3b0598ddc8e82668a77bce45150442a34c7ce3
Robi Nino <robi.nino@gmail.com>
2021-12-16 10:56:57 +0200

    Update xray build scan - pre cli fixes

--- tasks/JFrogXrayBuildScan/xrayBuildScan.js
+++ tasks/JFrogXrayBuildScan/xrayBuildScan.js
@@ -20,25 +20,30 @@ function RunTaskCbk(cliPath) {
         utils.configureDefaultXrayServer(serverId, cliPath, workDir);
     }
 
-    // Get input parameters
     let allowFailBuild = tl.getBoolInput('allowFailBuild', false);
 
-    let cliCommand = utils.cliJoin(cliPath, cliXrayBuildScanCommand, utils.quote(buildName), utils.quote(buildNumber), '--fail=false');
-    cliCommand = utils.addProjectOption(cliCommand);
+    let cliCommand = utils.cliJoin(cliPath, cliXrayBuildScanCommand, utils.quote(buildName), utils.quote(buildNumber), '--fail=false', '--format=json');
+
+    // Add watches source if provided.
+    let watchesSource = tl.getInput('watchesSource', false);
+    if (watchesSource !== "none") {
+        cliCommand = utils.addStringParam(cliCommand, watchesSource, watchesSource, true);
+    }
+    cliCommand = utils.addBoolParam(cliCommand, "vuln", "vuln");
     utils.addServerIdOption(cliCommand, serverId);
 
     try {
         let taskRes = utils.executeCliCommand(cliCommand, process.cwd(), 'pipe');
         console.log('Scan result:\n' + taskRes);
         let resJson = JSON.parse(taskRes);
-        let scanUrl = resJson['summary']['more_details_url'];
+        let scanUrl = resJson[0]['xray_data_url'];
         if (scanUrl) {
             persistScanUrl(scanUrl);
         }
 
         // Check if should fail build.
         if (allowFailBuild) {
-            let buildFailed = resJson['summary']['fail_build'];
+            let buildFailed = resJson[0]['violations'].length > 0;
             if (buildFailed) {
                 tl.setResult(tl.TaskResult.Failed, "Build scan returned 'Fail Build'.");
             }
