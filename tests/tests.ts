e7fa2c6b2aa8f3989763ab5e03804e3c7c20cc85
Robi Nino <robi.nino@gmail.com>
2021-12-20 15:20:25 +0200

    Fix gradle test

--- tests/tests.ts
+++ tests/tests.ts
@@ -599,7 +599,7 @@ describe('JFrog Artifactory Extension Tests', (): void => {
             TestUtils.isSkipTest('maven')
         );
     });
-
+// todo get latest extractor
     describe('Gradle Tests', (): void => {
         runSyncTest(
             'Gradle',
