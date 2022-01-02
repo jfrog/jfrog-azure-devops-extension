c60a7b98f37441427d108af4fcf4e30f17406d86
sverdlov93 <sverdlov93@gmail.com>
2022-01-02 13:24:58 +0200

    fix windows single quotation

--- jfrog-tasks-utils/utils.js
+++ jfrog-tasks-utils/utils.js
@@ -220,6 +220,9 @@ function executeCliCommand(cliCommand, runningDir, stdio) {
  * @returns {string}
  */
 function maskSecrets(str) {
+    if (isWindows()) {
+        str.replace(/--password=".*?"/g, '--password=***').replace(/--access-token=".*?"/g, '--access-token=***');
+    }
     return str.replace(/--password='.*?'/g, '--password=***').replace(/--access-token='.*?'/g, '--access-token=***');
 }
 
@@ -374,6 +377,9 @@ function cliJoin(...args) {
 }
 
 function quote(str) {
+    if (isWindows()){
+        return '"' + str + '"';
+    }
     return "'" + str + "'";
 }
 
