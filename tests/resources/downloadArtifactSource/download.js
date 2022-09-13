9745e660e83eb449dd48f67f3d4f29a8dd43c4c9
sverdlov93 <sverdlov93@gmail.com>
2022-09-13 12:36:55 +0300

    update docker readme

--- tests/resources/downloadArtifactSource/download.js
+++ tests/resources/downloadArtifactSource/download.js
@@ -6,7 +6,9 @@ const TEST_NAME = path.basename(__dirname);
 let inputs = {
     definition: 'downloadArtifactSourceBuild',
     version: '5',
-    downloadPath: testUtils.getLocalTestDir(TEST_NAME)
+    downloadPath: testUtils.getLocalTestDir(TEST_NAME),
+    flat: true,
+    threads: 4
 };
 
 testUtils.runArtifactoryTask(testUtils.generic, {}, inputs);
