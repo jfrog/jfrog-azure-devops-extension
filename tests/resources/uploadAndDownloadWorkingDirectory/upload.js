5494b0b0eeb3d52d32de211287f9cd1cc48915ff
David Gardiner <david@gardiner.net.au>
2023-06-04 18:10:34 +0930

    Add 'workingDirectory' parameter to the Generic Artifacts task (#402)

---
+++ tests/resources/uploadAndDownloadWorkingDirectory/upload.js
@@ -0,0 +1,23 @@
+const testUtils = require('../../testUtils');
+
+const TEST_NAME = testUtils.getTestName(__dirname);
+
+let inputs = {
+    command: 'Upload',
+    fileSpec: JSON.stringify({
+        files: [
+            {
+                pattern: './subdir/*',
+                target: testUtils.getRemoteTestDir(testUtils.getRepoKeys().repo1, TEST_NAME)
+            }
+        ]
+    }),
+    failNoOp: true,
+    dryRun: false,
+    insecureTls: false,
+    preserveSymlinks: false,
+    specSource: 'taskConfiguration',
+    workingDirectory: testUtils.getTestLocalFilesDir(__dirname)
+};
+
+testUtils.runArtifactoryTask(testUtils.generic, {}, inputs);
