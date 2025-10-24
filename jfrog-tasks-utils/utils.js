d38d2d4fd62f5907ae0e584f10b3b084acc99e4d
Naveen Kumar <naveenku@jfrog.com>
2025-10-24 14:02:41 +0530

    Updated plugin released version

--- jfrog-tasks-utils/utils.js
+++ jfrog-tasks-utils/utils.js
@@ -32,7 +32,7 @@ const minCustomCliVersion = '2.10.0';
 const minSupportedStdinSecretCliVersion = '2.36.0';
 const minSupportedServerIdEnvCliVersion = '2.37.0';
 const minSupportedOidcCliVersion = '2.75.0';
-const pluginVersion = '2.12.1';
+const pluginVersion = '2.12.2';
 const buildAgent = 'jfrog-azure-devops-extension';
 
 /**
