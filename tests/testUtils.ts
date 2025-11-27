25e03f0c4b55a5bc523453fb545c3be300f6c76d
Bhanu Reddy <bhanur@jfrog.com>
2025-11-27 20:43:37 +0530

    Fixed test failures

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -2,7 +2,7 @@ import * as mockRun from 'azure-pipelines-task-lib/mock-run';
 import * as tl from 'azure-pipelines-task-lib/task';
 import { join, basename } from 'path';
 import * as fs from 'fs-extra';
-import { rimraf, rimrafSync } from 'rimraf';
+import { rimraf } from 'rimraf';
 import * as syncRequest from 'sync-request';
 import * as assert from 'assert';
 import NullWritable from 'null-writable';
@@ -114,7 +114,7 @@ export function runTaskForService(testMain: string, variables: any, inputs: any)
 
 export function recreateTestDataDir(): void {
     if (fs.existsSync(testDataDir)) {
-        rimrafSync(testDataDir);
+        rimraf.sync(testDataDir);
     }
     fs.mkdirSync(testDataDir);
 }
@@ -144,8 +144,10 @@ export function cleanToolCache(): void {
 
 export function cleanUpAllTests(): void {
     if (fs.existsSync(testDataDir)) {
-        rimraf(testDataDir).catch((err: Error) => {
-            console.warn('Tests cleanup issue: ' + err);
+        rimraf(testDataDir, (err: Error | null | undefined): void => {
+            if (err) {
+                console.warn('Tests cleanup issue: ' + err);
+            }
         });
     }
     deleteTestRepositories();
