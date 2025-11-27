1089a5d0b112cfcf917e1d14956c50aa0f2e0803
Bhanu Reddy <bhanur@jfrog.com>
2025-11-27 19:33:04 +0530

    Fixed lint errors

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -2,7 +2,7 @@ import * as mockRun from 'azure-pipelines-task-lib/mock-run';
 import * as tl from 'azure-pipelines-task-lib/task';
 import { join, basename } from 'path';
 import * as fs from 'fs-extra';
-import * as rimraf from 'rimraf';
+import { rimraf, rimrafSync } from 'rimraf';
 import * as syncRequest from 'sync-request';
 import * as assert from 'assert';
 import NullWritable from 'null-writable';
@@ -114,7 +114,7 @@ export function runTaskForService(testMain: string, variables: any, inputs: any)
 
 export function recreateTestDataDir(): void {
     if (fs.existsSync(testDataDir)) {
-        rimraf.sync(testDataDir);
+        rimrafSync(testDataDir);
     }
     fs.mkdirSync(testDataDir);
 }
