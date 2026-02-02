270f44ce9750b8578105910da9e8b69b80086418
agrasth <agrasthn@jfrog.com>
2026-02-03 01:30:50 +0530

    Update utils.js

--- jfrog-tasks-utils/utils.js
+++ jfrog-tasks-utils/utils.js
@@ -142,7 +142,7 @@ const minCustomCliVersion = '2.10.0';
 const minSupportedStdinSecretCliVersion = '2.36.0';
 const minSupportedServerIdEnvCliVersion = '2.37.0';
 const minSupportedOidcCliVersion = '2.75.0';
-const pluginVersion = '2.12.5';
+const pluginVersion = '2.12.6';
 const buildAgent = 'jfrog-azure-devops-extension';
 
 /**
