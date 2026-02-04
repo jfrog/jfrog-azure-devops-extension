6906bfa15bc8e7fc092e0b265806e0b01d02ff9d
reshmifrog <reshmig@jfrog.com>
2026-02-04 13:05:15 +0530

    update plugin version (#584)

--- jfrog-tasks-utils/utils.js
+++ jfrog-tasks-utils/utils.js
@@ -141,7 +141,7 @@ const minCustomCliVersion = '2.10.0';
 const minSupportedStdinSecretCliVersion = '2.36.0';
 const minSupportedServerIdEnvCliVersion = '2.37.0';
 const minSupportedOidcCliVersion = '2.75.0';
-const pluginVersion = '2.12.6';
+const pluginVersion = '2.12.7';
 const buildAgent = 'jfrog-azure-devops-extension';
 
 /**
