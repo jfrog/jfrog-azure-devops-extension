60440c761263f4d11381e47b21f00fdbc0484f13
Kanishk Gupta <kanishkg@jfrog.com>
2026-04-21 18:31:06 +0530

    Update utils.js

--- jfrog-tasks-utils/utils.js
+++ jfrog-tasks-utils/utils.js
@@ -11,7 +11,7 @@ const semver = require('semver');
 const fileName = getCliExecutableName();
 const jfrogCliToolName = 'jf';
 const cliPackage = 'jfrog-cli-' + getArchitecture();
-const fallbackCliVersion = '2.89.0';
+const fallbackCliVersion = '2.99.0';
 let defaultJfrogCliVersion = '2.102.0';
 
 /**
